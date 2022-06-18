b36b149b261b61942152a59fb73c343154d2b54a
Asadbek <a.nosirjonov@uic.group>
2022-06-19 01:23:56 +0500

    feat cards, api, styles

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,7 +1,24 @@
-export default function Main() {
-  return(
-    <div className="container content">
-      All content
-    </div>
-  )
-} 
+import React from "react";
+import Movies from "../components/Movies";
+
+export default class Main extends React.Component {
+	state = {
+		movies: []
+	}
+
+	componentDidMount() {
+		fetch('http://www.omdbapi.com/?apikey=329ffa13&s=hacker')
+			.then((res) => res.json())
+			.then(data => this.setState({ movies: data.Search }))
+	}
+
+	render() {
+		return (
+			<div className="container content">
+				{this.state.movies.length ?
+					<Movies movies={this.state.movies} /> :
+					<h1>Loading...</h1>}
+			</div>
+		)
+	}
+}
