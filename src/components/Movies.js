c851b1251ddc62a4ffcec19f888fb114a1f720ad
Asadbek <a.nosirjonov@uic.group>
2022-06-19 01:26:00 +0500

    feat cards, api, styles

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Movie from './Movie'
 
 export default function Movies(props) {
-  const {movies} = props
+  const { movies } = props
   return (
     <div className='movies'>
       {movies.map(movie => (
