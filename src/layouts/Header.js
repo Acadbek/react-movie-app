8703f35113cb4f897b554efde53f757840482ea3
Asadbek <a.nosirjonov@uic.group>
2022-06-19 00:16:57 +0500

    feat

---
+++ src/layouts/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return(
+    <nav className="header #00796b teal darken-2">
+    <div class="nav-wrapper">
+      <a href="#!" className="brand-logo">Logo</a>
+    </div>
+  </nav>
+  )
+}
---
+++ src/layouts/Main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+  return(
+    <div className="container content">
+      All content
+    </div>
+  )
+} 
