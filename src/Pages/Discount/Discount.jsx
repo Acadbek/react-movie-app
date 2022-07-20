b40e964d9bc7702695043c7782db8cf37ac71f28
Asadbek <a.nosirjonov@uic.group>
2022-07-20 21:25:59 +0500

    feat: discount page

--- src/Pages/Discount/Discount.jsx
+++ src/Pages/Discount/Discount.jsx
@@ -7,6 +7,35 @@ import PackageCard from "../../Components/package/Package";
 import img from "../../assets/photo/sydney.jpg";
 
 const Discount = () => {
+  const data = [
+    {
+      img: img,
+      package: "Contact us 12%",
+      day: "8 day",
+      location: "Samarqand",
+      title: "TOUR TO SATORINI",
+      desc: "Malesuada incidunt excepturi proident quo eros? Id interd...",
+      price: "Price: 16,653,000 so'm or $1525 $1255",
+    },
+    {
+      img: img,
+      package: "Contact us 12%",
+      day: "8 day",
+      location: "Samarqand",
+      title: "TOUR TO SATORINI",
+      desc: "Malesuada incidunt excepturi proident quo eros? Id interd...",
+      price: "Price: 16,653,000 so'm or $1525 $1255",
+    },
+    {
+      img: img,
+      package: "Contact us 12%",
+      day: "8 day",
+      location: "Samarqand",
+      title: "TOUR TO SATORINI",
+      desc: "Malesuada incidunt excepturi proident quo eros? Id interd...",
+      price: "Price: 16,653,000 so'm or $1525 $1255",
+    },
+  ];
   return (
     <div>
       <InnerAndOuterContainer>
@@ -15,15 +44,20 @@ const Discount = () => {
         </GlobalContainer>
       </InnerAndOuterContainer>
       <GlobalContainer>
-        <PackageCard
-          background={img}
-          package="Aloqa10%"
-          day="8 day"
-          location="Samarqand"
-          title="TOUR TO SATORINI"
-          desc="Malesuada incidunt excepturi proident quo eros? Id interd..."
-          price="Price: 1,092 so'm or $0.1 $0.001"
-        />
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 md:gap-8 mt-[95px] mb-[95px]">
+        {data.map((item) => (
+          <PackageCard
+            className="col-span-6"
+            background={item.img}
+            package={item.package}
+            day={item.day}
+            location={item.location}
+            title={item.title}
+            desc={item.desc}
+            price={item.price}
+          />
+        ))}
+      </div>
       </GlobalContainer>
     </div>
   );
