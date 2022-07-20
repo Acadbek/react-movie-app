9741e2bc83776cb1bd7fa02a049593e8b7a1bef7
Asadbek <a.nosirjonov@uic.group>
2022-07-20 14:48:39 +0500

    feat: pages

--- src/App.jsx
+++ src/App.jsx
@@ -26,58 +26,57 @@ function App() {
   const data = [
     {
       img: backgroundPackage,
-      package: 'Contact us 12%',
-      day: '8 day',
-      location: 'Samarqand',
-      title: 'TOUR TO SATORINI',
-      desc: 'Malesuada incidunt excepturi proident quo eros? Id interd...',
-      price: "Price: 16,653,000 so'm or $1525 $1255"
+      package: "Contact us 12%",
+      day: "8 day",
+      location: "Samarqand",
+      title: "TOUR TO SATORINI",
+      desc: "Malesuada incidunt excepturi proident quo eros? Id interd...",
+      price: "Price: 16,653,000 so'm or $1525 $1255",
     },
     {
       img: backgroundPackage,
-      package: 'Contact us 12%',
-      day: '8 day',
-      location: 'Samarqand',
-      title: 'TOUR TO SATORINI',
-      desc: 'Malesuada incidunt excepturi proident quo eros? Id interd...',
-      price: "Price: 16,653,000 so'm or $1525 $1255"
+      package: "Contact us 12%",
+      day: "8 day",
+      location: "Samarqand",
+      title: "TOUR TO SATORINI",
+      desc: "Malesuada incidunt excepturi proident quo eros? Id interd...",
+      price: "Price: 16,653,000 so'm or $1525 $1255",
     },
     {
       img: backgroundPackage,
-      package: 'Contact us 12%',
-      day: '8 day',
-      location: 'Samarqand',
-      title: 'TOUR TO SATORINI',
-      desc: 'Malesuada incidunt excepturi proident quo eros? Id interd...',
-      price: "Price: 16,653,000 so'm or $1525 $1255"
+      package: "Contact us 12%",
+      day: "8 day",
+      location: "Samarqand",
+      title: "TOUR TO SATORINI",
+      desc: "Malesuada incidunt excepturi proident quo eros? Id interd...",
+      price: "Price: 16,653,000 so'm or $1525 $1255",
     },
-  ]
+  ];
   return (
     <div>
       <Navbar />
       <Container>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
           {array.map((item) => (
-            <div>
-              <Card
-                background={item.img}
-                location={item.location}
-                title={item.title}
-              />
-            </div>
+            <Card
+              background={item.img}
+              location={item.location}
+              title={item.title}
+            />
           ))}
         </div>
         <div className="grid grid-cols-2 gap-8">
           {data.map((item) => (
-              <PackageCard className="col-span-6"
-                background={item.img}
-                package={item.package}
-                day={item.day}
-                location={item.location}
-                title={item.title}
-                desc={item.desc}
-                price={item.price}
-              />
+            <PackageCard
+              className="col-span-6"
+              background={item.img}
+              package={item.package}
+              day={item.day}
+              location={item.location}
+              title={item.title}
+              desc={item.desc}
+              price={item.price}
+            />
           ))}
         </div>
       </Container>
