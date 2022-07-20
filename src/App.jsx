cdff677dd1b39596a6c3a916214c1d9424897426
Asadbek <a.nosirjonov@uic.group>
2022-07-20 12:08:02 +0500

    feat: card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { Container } from "./main-styles/styles";
 import Card from "./components/card";
 import Navbar from "./components/navbar";
+import background from "./assets/photos/card1.jpg";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Card />
+      <Container className="grid grid-cols-12 gap-12">
+        <div className="col-span-4">
+          <Card
+            background={background}
+            location="gavayi orollari"
+            title="dengiz bo'ylari va manzaralari"
+          />
+        </div>
+        <div className="col-span-4">
+          <Card
+            background={background}
+            location="gavayi orollari"
+            title="dengiz bo'ylari va manzaralari"
+          />
+        </div>
+        <div className="col-span-4">
+          <Card
+            background={background}
+            location="gavayi orollari"
+            title="dengiz bo'ylari va manzaralari"
+          />
+        </div>
+      </Container>
     </div>
   );
 }
