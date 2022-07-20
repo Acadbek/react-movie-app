3cfc5cfc1c91af62093611adccc18165f9193c56
Asadbek <a.nosirjonov@uic.group>
2022-07-20 21:03:47 +0500

    fix

--- src/Pages/Discount/Discount.jsx
+++ src/Pages/Discount/Discount.jsx
@@ -3,12 +3,29 @@ import {
   GlobalContainer,
   InnerAndOuterContainer,
 } from "../../styles/GlobalStyle.styled";
+import PackageCard from "../../Components/package/Package";
+import img from "../../assets/photo/sydney.jpg";
 
 const Discount = () => {
   return (
-    <InnerAndOuterContainer>
-      <GlobalContainer><h1>CHEGIRMALAR</h1></GlobalContainer>
-    </InnerAndOuterContainer>
+    <div>
+      <InnerAndOuterContainer>
+        <GlobalContainer>
+          <h1>CHEGIRMALAR</h1>
+        </GlobalContainer>
+      </InnerAndOuterContainer>
+      <GlobalContainer>
+        <PackageCard
+          background={img}
+          package="Aloqa10%"
+          day="8 day"
+          location="Samarqand"
+          title="TOUR TO SATORINI"
+          desc="Malesuada incidunt excepturi proident quo eros? Id interd..."
+          price="Price: 1,092 so'm or $0.1 $0.001"
+        />
+      </GlobalContainer>
+    </div>
   );
 };
 
